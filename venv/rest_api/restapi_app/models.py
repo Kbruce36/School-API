056a1f73412ad2943a3f3c775dbbc3f0018c804b
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-08-03 01:42:20 +0300

    modified model names

--- venv/rest_api/restapi_app/models.py
+++ venv/rest_api/restapi_app/models.py
@@ -9,7 +9,8 @@ class StaffMember(models.Model):
     gender = models.CharField(max_length=250)
     age = models.CharField(max_length=250)
     email = models.CharField(max_length=250)
-
+    def __str__(self):
+        return self.first_name+" "+self.second_name
 
 
 class Student(models.Model):
@@ -17,6 +18,8 @@ class Student(models.Model):
     second_name = models.CharField(max_length=250)
     gender = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.first_name+" "+self.second_name
 
 
 class Course(models.Model):
@@ -24,15 +27,19 @@ class Course(models.Model):
     course_code = models.CharField(max_length=50)
     department = models.CharField(max_length=50)
     duration = models.CharField(max_length=50)
-
+    def __str__(self):
+        return self.course_name
 
 class Department(models.Model):
     department_name = models.CharField(max_length=50)
     staff = models.CharField(max_length=250)
     head_of_department = models.CharField(max_length=250)
     courses_in_department = models.CharField(max_length=250)
-
+    def __str__(self):
+        return self.department_name
 
 class Club(models.Model):
     club_name = models.CharField(max_length=50)
     head_of_club = models.CharField(max_length=250)
+    def __str__(self):
+        return self.club_name
