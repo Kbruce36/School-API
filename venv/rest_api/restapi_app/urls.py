8077df8899c865a95ba15a38f375f1866180ab89
paulie <pktpaulie@gmail.com>
2023-08-03 00:51:43 +0300

    Add Departments and Clubs

--- venv/rest_api/restapi_app/urls.py
+++ venv/rest_api/restapi_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('staff/', views.staff_list),
     path('staff/<int:pk>', views.staff_detail),
-
+    path('departments/', views.department_list),
+    path('departments/<int:pk>', views.department_detail),
+    path('clubs/', views.club_list),
+    path('clubs/<int:pk>', views.club_detail),
 ]
--- venv/rest_api/restapi_app/views.py
+++ venv/rest_api/restapi_app/views.py
@@ -35,3 +35,64 @@ def staff_detail(request,pk):
     elif request.method == 'DELETE':
         staff.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Departments Views
+
+@api_view(['POST','GET'])
+def department_list(request):
+    if request.method == 'POST':
+        serializer = DepartmentSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'GET':
+        department = Department.objects.all()
+        serializer = StaffSerializer(department, many=True)
+        return Response(serializer.data)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def department_detail(request,pk):
+    department = Department.objects.get(pk=pk)
+    if request.method == 'GET':
+        serializer = DepartmentSerializer(department)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = DepartmentSerializer(department, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        department.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Clubs Views
+
+@api_view(['POST','GET'])
+def club_list(request):
+    if request.method == 'POST':
+        serializer = ClubSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'GET':
+        club = Club.objects.all()
+        serializer = ClubSerializer(club, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def club_detail(request,pk):
+    club = Club.objects.get(pk=pk)
+    if request.method == 'GET':
+        serializer = ClubSerializer(club)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ClubSerializer(club, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        club.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
