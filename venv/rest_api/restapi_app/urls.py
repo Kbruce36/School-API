675dcd115297fd9ff175c868cbeccc1cb56316fc
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-08-03 01:29:58 +0300

    modified urls

--- venv/rest_api/restapi_app/urls.py
+++ venv/rest_api/restapi_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('staff/', views.staff_list),
     path('staff/<int:pk>', views.staff_detail),
-    path('student/', views.student_list),
-    path('student/<int:pk>', views.student_detail),
+    path('students/', views.student_list),
+    path('students/<int:pk>', views.student_detail),
     path('departments/', views.department_list),
     path('departments/<int:pk>', views.department_detail),
     path('clubs/', views.club_list),
