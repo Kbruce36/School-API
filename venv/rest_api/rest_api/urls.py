875f6e4a8a71bfcab07941efc9e438f71bbfad43
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-08-04 07:33:59 +0300

    School API api_info file modified

--- venv/rest_api/rest_api/urls.py
+++ venv/rest_api/rest_api/urls.py
@@ -33,12 +33,12 @@ urlpatterns = [
 # Swagger Configuration
 schema_view = get_schema_view(
     openapi.Info(
-        title="MY DJANGO FOOD API WITH SWAGGER",
-        default_version='v1.1',
-        description="TEsting Food API and Swagger",
-        terms_of_service="https://your-terms-of-service-url.com/",
-        contact=openapi.Contact(email="kitemaggwashafic@gmail.com"),
-        license=openapi.License(name="Your License"),
+        title="SCHOOL DJANGO API WITH SWAGGER",
+        default_version='v1.0',
+        description="TEsting School Django API and Swagger",
+        terms_of_service="https://your-terms-of-bootcamp12-python.com/",
+        contact=openapi.Contact(email="teampy@gmail.com"),
+        license=openapi.License(name="License Bootcamp13 Team Python"), 
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),  # Set appropriate permissions here
--- venv/rest_api/restapi_app/api_info.py
+++ venv/rest_api/restapi_app/api_info.py
@@ -4,8 +4,8 @@ api_info = openapi.Info(
     title="SCHOOL DJANGO API WITH SWAGGER",
     default_version='v1.0',
     description="TEsting School Django API and Swagger",
-    terms_of_service="https://your-terms-of-service-url.com/",
+    terms_of_service="https://your-terms-of-bootcamp12-python.com/",
     contact=openapi.Contact(email="teampy@gmail.com"),
-    license=openapi.License(name="Your License"), 
+    license=openapi.License(name="License Bootcamp13 Team Python"), 
 )
 
