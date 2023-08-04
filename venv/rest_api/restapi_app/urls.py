f6a0676ffc24643d4169b89b214b756fddf76305
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-08-04 09:15:45 +0300

    swagger

--- venv/rest_api/restapi_app/urls.py
+++ venv/rest_api/restapi_app/urls.py
@@ -1,6 +1,25 @@
 from django.urls import path
 from . import views
 
+#######################*******
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('staff/', views.staff_list),
     path('staff/<int:pk>', views.staff_detail),
@@ -12,4 +31,6 @@ urlpatterns = [
     path('clubs/<int:pk>', views.club_detail),
     path('courses/', views.course_list),
     path('courses/<int:pk>', views.course_detail),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
